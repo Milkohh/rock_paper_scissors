6ad66818a6e52ef95699a38254682b4b70586b3c
Brendan Miller <brendan.miller261@gmail.com>
2021-04-13 01:12:52 +1000

    Implement function to declare winner

--- script.js
+++ script.js
@@ -4,3 +4,38 @@ function computerPlay() {
     const selection = Math.floor(Math.random() * 3);
     return choices[selection];
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerWin = false;
+    computerWin = false;
+
+    // Rock is selected
+    if (playerSelection == "Rock" && computerSelection == "Paper") {
+        computerWin = true;
+    } else if (playerSelection == "Rock" && computerSelection == "Scissors") {
+        playerWin = true;
+    }
+
+    // Scissors is selected
+    else if (playerSelection == "Scissors" && computerSelection == "Rock") {
+        computerWin = true;
+    } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+        playerWin = true;
+    }
+
+    // Paper is selected
+    else if (playerSelection == "Paper" && computerSelection == "Scissors") {
+        computerWin = true;
+    } else if (playerSelection == "Paper" && computerSelection == "Rock") {
+        playerWin = true;
+    }
+
+    // Check who won and return the resulting message
+    if (computerWin) {
+        return `You Lose ${computerSelection} beats ${playerSelection}`;
+    } else if (playerWin) {
+        return `You Win ${playerSelection} beats ${computerSelection}`;
+    } else {
+        return `You Draw ${playerSelection} is the same as ${computerSelection}`;
+    }
+}
